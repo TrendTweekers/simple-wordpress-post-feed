4e4c84f07fdcf59fe6b3dc5d16b36fc35ad1d45b
Flafi87 <laaszlo.gaal@gmail.com>
2019-09-30 18:49:12 +0200

    get,redact,uninstall- route

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ import Router from "koa-router";
 import session from "koa-session";
 import getSubscriptionStatus from "./lib/firebase/getSubscriptionStatus";
 import { receiveWebhook } from "@shopify/koa-shopify-webhooks";
-import * as handlers from "./handlers/index";
+
+// Routes
+import { redactRoute } from "./routes/redactRoute";
+import { uninstallRoute } from "./routes/uninstallRoute";
 
 const env = require("./config/config");
 const {
@@ -66,6 +69,12 @@ app.prepare().then(() => {
     })
   );
   const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+
+  router
+    .get("/api/data", getRoute)
+    .post("/api/redact", webhook, redactRoute)
+    .post("/api/uninstall", webhook, uninstallRoute);
+
   router.get("*", verifyRequest(), async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
