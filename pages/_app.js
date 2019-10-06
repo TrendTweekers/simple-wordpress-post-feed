5bc9d910c880f234ccebacaa1bb821d10d6f6a13
Flafi87 <laaszlo.gaal@gmail.com>
2019-10-06 14:14:40 +0200

    fix fetch

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ import { Provider } from "@shopify/app-bridge-react";
 import Cookies from "js-cookie";
 import "@shopify/polaris/styles.css";
 import React from "react";
+import fetch from "node-fetch";
+import "cross-fetch/polyfill";
 
 import { SHOPIFY_API_KEY } from "../server/config/config";
 
