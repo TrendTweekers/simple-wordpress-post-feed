385b557e7a08dc2fb03a0880f6bb810097afcbf9
Flafi87 <laaszlo.gaal@gmail.com>
2019-10-06 15:19:15 +0200

    fix fetch

--- server/routes/installSectionRoute.js
+++ server/routes/installSectionRoute.js
@@ -2,6 +2,8 @@ const { PubSub } = require("@google-cloud/pubsub");
 const { db } = require("../lib/firebase/firebase");
 const config = require("../config/config");
 
+const pubsub = new PubSub();
+
 const { COLLECTION } = config;
 
 /** Getting all the data from DB
@@ -9,7 +11,7 @@ const { COLLECTION } = config;
  */
 exports.installSectionRoute = async ctx => {
   const { shop } = await ctx.session;
-  const pubsub = new PubSub();
+
   const topicName = "shopify";
 
   const shopRef = db.collection(COLLECTION).doc(shop);
@@ -25,14 +27,16 @@ exports.installSectionRoute = async ctx => {
         // Add two custom attributes, origin and username, to the message
         const customAttributes = {
           app: "wordpress-shopify",
-          shop: docu.shop,
-          token: docu.token,
-          mainThemeId: docu.themeId
+          shop: toString(docu.shop),
+          token: toString(docu.token),
+          mainThemeId: toString(docu.themeId)
         };
+
+        const data = Buffer.from("Hello, world!");
 
         const messageId = await pubsub
           .topic(topicName)
-          .publish(customAttributes);
+          .publish(data, customAttributes);
         console.log(`Message ${messageId} published.`);
         // /
         return docu.email;
