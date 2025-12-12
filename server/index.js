47e3b908f2e1872c005603b4b0184c23c8cdd13f
trendtweakers <trendtweakers00@gmail.com>
2025-12-12 19:46:30 +0100

    Fix Railway start command: ensure Koa server boots instead of Next.js, add boot logging

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+console.log("BOOT: Koa server/index.js starting, NODE_ENV=", process.env.NODE_ENV);
+console.log("BOOT: Custom Koa server will handle all routes including /install/auth");
+
 require('@babel/register')({
   presets: ['@babel/preset-env'],
   ignore: ['node_modules'],
